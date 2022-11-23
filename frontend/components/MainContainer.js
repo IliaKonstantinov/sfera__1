13e87263e65db772059b2ea0bb0bc14454f116b7
Ilia Konstantinov <steam_ilya73@bk.ru>
2022-11-23 15:39:59 +0400

    resolve conflicts

--- frontend/components/MainContainer.js
+++ frontend/components/MainContainer.js
@@ -1,6 +1,5 @@
 import Head from "next/head";
 import A from "./A";
-<<<<<<< HEAD
 import styles from "../styles/MainContainer.module.css";
 import { useRouter } from "next/router";
 import { en, ru } from "../translations";
@@ -34,32 +33,6 @@ const MainContainer = (props) => {
   }
 
 
-=======
-import style from "../styles/MainContainer.module.css";
-import { ru, en} from "../translations";
-import { useRouter } from "next/router";
-
-const MainContainer = ({ children, title }) => {
-
-  const router = useRouter();
-
-  const { locale } = router;
-
-  let t = en;
-
-  switch(locale){
-    case "ru":
-      t = ru;
-      break;
-    case "en":
-      t = en;
-      break;
-  }
-
-  const handleChangeLang = (lang) => {
-    router.push("/", "/", {locale: lang})
-  }
->>>>>>> a1e1d319776188a0fca7698e061ecb825069bd1f
   return (
     <>
       <Head>
@@ -67,7 +40,6 @@ const MainContainer = ({ children, title }) => {
         <meta name="description" content="auth-project" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-<<<<<<< HEAD
       <div
         className={!props.toggleChangeStyle ? styles.nav : styles.nav_style1}
       >
@@ -82,22 +54,10 @@ const MainContainer = ({ children, title }) => {
           text={t.registration}
         ></A>
         <button onClick={onChangeStyles}>Change style</button>
-<<<<<<< HEAD
         <div className={!props.toggleChangeStyle ? styles.locales : ''}>
           <button className={!props.toggleChangeStyle ? styles.locale1 : styles.locale} onClick={() => handleChangeLang("ru")}>Русский</button>
           <button className={!props.toggleChangeStyle ? styles.locale1 : styles.locale} onClick={() => handleChangeLang("en")}>English</button>
         </div>
-=======
-=======
-      <div className={style.nav}>
-        <A href="/" text={t.login}></A>
-        <A href="/registration" text={t.registration}></A>
-        <div>
-          <button className={style.locale} onClick={() => handleChangeLang("ru")}>Русский</button>
-          <button className={style.locale} onClick={() => handleChangeLang("en")}>English</button>
-        </div>
->>>>>>> a1e1d319776188a0fca7698e061ecb825069bd1f
->>>>>>> 0f660e1fc827f10859f0e68c9dd6ffb533752176
       </div>
       <div>{props.children}</div>
     </>
