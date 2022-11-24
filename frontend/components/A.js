9539c43a9a24bf839045ffa811baa9caee85cd97
AnKhudyakov <andrew_job@list.ru>
2022-11-24 20:06:39 +0400

    add scss

--- frontend/components/A.js
+++ frontend/components/A.js
@@ -1,18 +1,20 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
-import styles from "../styles/A.module.css";
+import styles from "../styles/A.module.scss";
 import { connect, useDispatch, useSelector } from "react-redux";
 
 function A({ text, href, toggleChangeStyle }) {
   const { pathname } = useRouter();
 
   return (
-      <Link
-        className={pathname == href ? styles.active : styles.link_wrapper}
-        href={href}
-      >
-        <div className={!toggleChangeStyle ? styles.link_style1 : styles.link}>{text}</div>
-      </Link>
+    <Link
+      className={pathname == href ? styles.active : styles.link_wrapper}
+      href={href}
+    >
+      <div className={!toggleChangeStyle ? styles.link_style1 : styles.link}>
+        {text}
+      </div>
+    </Link>
   );
 }
 
